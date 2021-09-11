3c2fce9b9ece0c20d9749a8811f31ee770df5af9
Gustavo Duarte <gduartemenezes@gmail.com>
2021-09-11 16:45:23 -0300

    feat: create a basic service

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,10 @@
+/**
+ * name - string
+ * duration - number
+ * educator - string
+ */
+class CreateCourseService {
+    execute(name: string, duration: number, educator: string) {
+        console.log(name, duration, educator)
+    }
+}
